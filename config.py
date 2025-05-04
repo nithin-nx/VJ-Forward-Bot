5994114fb2986f29b9f8b5ae5af6c625ac3b8359
nithin-nx <www.nithinnibin@gmail.com>
2025-05-04 08:46:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    API_ID = int(environ.get("API_ID", "20393417"))
+    API_HASH = environ.get("API_HASH", "d4b6be89670498d4793794a882796de2")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7724957846:AAH18ByOiZ1hF9f_tsBetVXQnTIJm4qf1oY") 
+    BOT_SESSION = environ.get("BOT_SESSION", "NiCoBot") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://nithinnibin:Nithinnibin@cluster0.yox42ct.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "1778630011"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
